739abe52a023a02080ccbea55ed2e88a9a0c68d1
Artem Voronyi <voronyi.artem@gmail.com>
2023-08-20 19:07:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig) {  
+  const pars = new Map(bracketsConfig); 
+  const arrayBrackets = []; 
+  for (const brackConfig of str) {
+    
+    if (pars.get(arrayBrackets.at(-1)) == brackConfig) {
+      
+      arrayBrackets.pop(); 
+    } else {
+      
+      arrayBrackets.push(brackConfig); 
+    }
+  }
+  return arrayBrackets.length == 0; 
 }
