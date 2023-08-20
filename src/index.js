18328629f8750e57b13cd2901c867670d97d9ccd
Artem Voronyi <voronyi.artem@gmail.com>
2023-08-20 19:08:01 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ module.exports = function check(str, bracketsConfig) {
   const pars = new Map(bracketsConfig); 
   const arrayBrackets = []; 
   for (const brackConfig of str) {
-    
     if (pars.get(arrayBrackets.at(-1)) == brackConfig) {
-      
       arrayBrackets.pop(); 
     } else {
-      
       arrayBrackets.push(brackConfig); 
     }
   }
